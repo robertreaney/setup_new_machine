aed3c532bdbe12989ead2729eec3085dcd4b0ad7
robertreaney <robertlreaney@gmail.com>
2024-09-10 12:01:39 -0400

    initiall commit

---
+++ README.md
@@ -0,0 +1,10 @@
+0. enable "show file extensions" and "show hidden files" in windows start menu
+1. Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+2. Install vscode `https://visualstudio.microsoft.com/downloads/`
+    - select "open with code" directory context on install menu
+3. install wsl `https://learn.microsoft.com/en-us/windows/wsl/install`
+    - use the `wsl_setup.sh` script after getting your linux dist installed
+4. install vscode extensions with this command
+    `Get-Content extensions.txt | ForEach-Object { code --install-extension $_ }`
+5. `https://dbeaver.io/download/`
+6. `https://www.rarlab.com/download.html`
---
+++ wsl_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# python
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y software-properties-common unzip python3-pip python3.10-venv gnupg
+sudo apt update && sudo apt upgrade -y
+echo 'alias python=python3' >> ~/.bashrc && source ~/.bashrc
+
+# aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+sudo apt update
+
+# install terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
